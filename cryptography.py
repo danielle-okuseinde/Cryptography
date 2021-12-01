0a3aa1e001ad71fec882f379a2c5afdd69fa5e9d
o0907236 <89143520+o0907236@users.noreply.github.com>
2021-12-01 08:05:09 -0600

    fixed affine T-T finally

--- cryptography.py
+++ cryptography.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def ciphers():
+def home():
     return render_template('cryptography.html')
+
 
 @app.route('/affine', methods=['POST', 'GET'])
 def affine():
@@ -20,8 +21,10 @@ def affine():
     if request.method == 'POST':
         plaintext = request.form.get('plaintext')
         plaintext = plaintext.upper()
+        ciphertext = "test"
         a = request.form.get('a')
         b = request.form.get('b')
+        temp = {}
 
         if not a.isdigit() or not b.isdigit():
             return render_template('affine.html', error="Your a and b values are invalid. Try again!")
@@ -29,16 +32,29 @@ def affine():
             ciphertext = affine_encrypt(plaintext, int(a), int(b))
             crib = ciphertext[1]
             ciphertext = ciphertext[0]
-            temp = {}
             for key in crib:
-                temp[key] = crib[key][1]
-            return render_template('affine.html', plaintext=plaintext, ciphertext=ciphertext, crib=temp)
+                temp[key] = crib[key][0]
+
+            return render_template('affine.html', plaintext="Plaintext: "+plaintext, ciphertext="Ciphertext: "+ciphertext, crib=temp)
 
     return render_template('affine.html')
 
-@app.route('/affine/encryption', methods=['POST', 'GET'])
-def affine_encryption():
-    pass
+def affine_encrypt(plaintext, a, b):
+    crib = reset_crib()
+    for key in crib:
+        val = crib[key][1]
+        val = (val*a+b)%26
+        crib[key] = (chr(val+65), val)
+    
+    array = [x for x in plaintext]
+    
+    ciphertext = ""
+
+    for letter in array:
+        if letter in crib:
+            ciphertext+=crib[letter][0]
+    
+    return ciphertext, crib
 
 @app.route('/caesar', methods=['GET'])
 def caesar():
@@ -52,29 +68,17 @@ def vigenere():
 def aristocrat():
     pass
 
+def reset_crib():
+    crib = {}
+    n = 0
+    for n in range(26):
+        crib[chr(n+65)] = (chr(n+65), n)
+    return crib
+
 @app.errorhandler(404)
 def err_404(e):
     return "Couldn't find page", 404
 
-
-def reset_crib():
-    crib = {}
-    n = 0
-    while n <= 90:
-        crib[chr(n+65)] = (chr(n+65), n)
-    return crib
-
-def affine_encrypt(plaintext, a, b):
-    crib = reset_crib()
-    for key in crib:
-        val = crib[key][1]
-        val = (val*a+b)%26
-        crib[key] = (chr(val+65), val)
-    
-    for letter in plaintext:
-        letter = crib[letter]
-    
-    return plaintext, crib
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=3001, debug=True)
