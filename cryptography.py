7e292a718df40aebc7ca9dfd9ddfeda5ece916d7
o0907236 <89143520+o0907236@users.noreply.github.com>
2021-12-02 08:04:43 -0600

    Update cryptography.py

--- cryptography.py
+++ cryptography.py
@@ -21,12 +21,11 @@ def affine():
     if request.method == 'POST':
         plaintext = request.form.get('plaintext')
         plaintext = plaintext.upper()
-        ciphertext = "test"
         a = request.form.get('a')
         b = request.form.get('b')
         temp = {}
 
-        if not a.isdigit() or not b.isdigit():
+        if not a.isdigit() or not b.isdigit() or a%1 == 0 or b%1 == 0:
             return render_template('affine.html', error="Your a and b values are invalid. Try again!")
         else:
             ciphertext = affine_encrypt(plaintext, int(a), int(b))
@@ -40,6 +39,7 @@ def affine():
     return render_template('affine.html')
 
 def affine_encrypt(plaintext, a, b):
+    # maybe change tuple of crib since i could always cast it
     crib = reset_crib()
     for key in crib:
         val = crib[key][1]
@@ -58,9 +58,75 @@ def affine_encrypt(plaintext, a, b):
     
     return ciphertext, crib
 
-@app.route('/caesar', methods=['GET'])
+@app.route('/caesar', methods=['POST', 'GET'])
 def caesar():
-    return 'Caesar'
+    # shift
+    # idea: add radio inputs to switch from encrypt to decrypt
+      if request.method == 'POST':
+        plaintext = request.form.get('plaintext')
+        plaintext = plaintext.upper()
+        shift = request.form.get('shift')
+        temp = {}
+
+        if not shift.isdigit() or not shift%1 == 0:
+            return render_template('caesar.html', error="Your shift value is invalid. Try again!")
+        else:
+            if request.POST['action'] == 'encrypt':
+                ciphertext = caesar_cipher(plaintext, shift, True)
+                crib = ciphertext[1]
+                ciphertext = ciphertext[0]
+
+            elif request.POST['action'] == 'decrypt':
+                ciphertext = plaintext
+                plaintext = caesar_cipher(plaintext, shift, False)
+                crib = plaintext[1]
+                plaintext = plaintext[0]
+
+            for key in crib:
+                temp[key] = crib[key][0]
+                
+
+            return render_template('caesar.html', plaintext="Plaintext: "+plaintext, ciphertext="Ciphertext: "+ciphertext, crib=temp)
+
+        # huh
+        # this is for radio inputs later; need to work out logic
+        if request.POST['action'] == 'decrypt':
+             return render_template('caesar.html', decrypt=True)
+
+        if len(request.form.get('plaintext')) > 0:
+            pass # if theres something in the form submitted we need to render template for ec and answer
+
+        if len(request.form.get('ciphertext')) > 0:
+            pass # if theres something in the form submitted we need to render template for dc and answer
+
+        else:
+            return render_template('caesar.html', encrypt=True)
+
+def caesar_cipher(plaintext, shift, encrypt):
+    # maybe change tuple of crib since i could always cast it
+    crib = reset_crib()
+    for key in crib:
+        val = crib[key][1]
+
+        if encrypt:
+            val = val+shift
+        elif not encrypt:
+            val = val-shift
+
+        crib[key] = (chr(val+65), val)
+    
+    array = [x for x in plaintext]
+    
+    ciphertext = ""
+
+    for letter in array:
+        if letter in crib:
+            ciphertext+=crib[letter][0]
+        else:
+            ciphertext+=letter
+    
+    return ciphertext, crib
+
 
 @app.route('/vigenere', methods=['GET'])
 def vigenere():
